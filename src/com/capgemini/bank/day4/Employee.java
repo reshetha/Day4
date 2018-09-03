39d6f0af42566ae2a78d3690346eea9f93e219a4
reshek <reshek@IN-MUM-LDVM008.corp.capgemini.com>
2018-09-03 05:29:55 -0400

    day4

---
+++ src/com/capgemini/bank/day4/Employee.java
@@ -0,0 +1,29 @@
+package com.capgemini.bank.day4;
+
+public class Employee 
+{
+	private int employeeId;
+	private String employeeName;
+	private int employeeAddress;
+	private long employeePhone;
+	private double basicSalary;
+	public final double specialAllowance=250.80;
+	public final double hra=1000.50;
+	
+	
+	public Employee(){
+	}
+	Employee( int employeeId, String employeeName,int employeeAddress,long employeePhone)
+	{
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeAddress = employeeAddress;
+		this.employeePhone = employeePhone;
+	}
+	public static calculateSalary(double salary)
+	{
+		
+		salary =basicSalary+(basicSa)
+	}
+}
+
